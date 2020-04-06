095e3ff1e8db9d155c71f7e86f8b94a36f2546d8
Cristian Torres <cristianlp.torr@gmail.com>
2020-04-05 21:38:42 -0300

    configuraciones--

---
+++ src/api/Configuration/Validator/RequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OperacionesApi.Configuration
+{
+    /// <summary>
+    /// Clase que se encarga de validar el path de cada request
+    /// para agregarlo al conjunto de metricas
+    /// </summary>
+    public class RequestValidator
+    {
+        public static bool validate(string path)
+        {
+            var conditionRequestNoMetrics = (path != "/metrics");
+            var conditionRequestResultados = path.Contains("resultados");
+            return conditionRequestNoMetrics && conditionRequestResultados;
+        } 
+    }
+}

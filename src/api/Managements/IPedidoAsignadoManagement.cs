ea9ef97f63aaa9b72211fc9966222b5e494fcd35
Cristian Torres <cristianlp.torr@gmail.com>
2020-04-16 15:31:57 -0300

    nuevo endpoint que muestra todas las operaciones

--- src/api/Managements/IPedidoAsignadoManagement.cs
+++ src/api/Managements/IPedidoAsignadoManagement.cs
@@ -1,16 +1,16 @@
-﻿using Andreani.Integracion.Eventos.Almacenes;
-using Infra.EventBus;
+﻿
 using OperacionesApi.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace OperacionesApi.Managements
 {
     public interface IPedidoAsignadoManagement
     {
-        void Guardar(Operacion operacion);
-        void Publicar(Operacion operacion);
+        
+        public void Guardar(Operacion operacion);
+        public void Publicar(Operacion operacion);
+        public IList<Operacion> ListarOperaciones();
     }
 }
